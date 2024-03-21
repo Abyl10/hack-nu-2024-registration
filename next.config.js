6df12323f1fe8a802b53c2b646ee987da8a7ce71
Abyl10 <abylay.kairatbek@gmail.com>
2024-03-21 17:47:55 +0500

    fix: asset prefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  assetPrefix: "https://hack-nu-2024-registration.vercel.app/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
